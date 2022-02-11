28b265f3b57b35cf32bcf115cc9f16c61efd8c2e
AbdElRahman Refaat <abdelrahmanrefaat.m@gmail.com>
2022-02-11 12:49:13 +0200

    User can now send messages and Bot responses with a simple message

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import java.util.ArrayList;
 
@@ -20,10 +24,27 @@ public class MainActivity extends AppCompatActivity {
         recycler.setLayoutManager(new LinearLayoutManager(this));
         // dummy data
         ArrayList<ChatMessageModel> arr = new ArrayList<>();
-        arr.add(new ChatMessageModel("Hello there! I am User Chan", 1));
-        arr.add(new ChatMessageModel("HI User Chan! I am your BOT Chan", 2));
-        arr.add(new ChatMessageModel("Nice meeting you BOT Chan",1));
-        arr.add(new ChatMessageModel("You too User Chan!",2));
+        arr.add(new ChatMessageModel("Hello there! I am User", 1));
+        arr.add(new ChatMessageModel("HI User! I am your BOT", 2));
+        arr.add(new ChatMessageModel("Nice meeting you BOT",1));
+        arr.add(new ChatMessageModel("You too User!",2));
         adapter.setList(arr);
+        EditText editTextMessage = (EditText) findViewById(R.id.edit_text_message);
+        ImageButton btnSend = (ImageButton) findViewById(R.id.btn_send_message);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String userMsg = editTextMessage.getText().toString();
+                if(userMsg.equals(""))
+                    return ;
+                arr.add(new ChatMessageModel(userMsg, 1));
+                editTextMessage.setText("");
+                final String botMsg = "Ok";
+                arr.add(new ChatMessageModel(botMsg, 2));
+                adapter.setList(arr);
+
+            }
+        });
+
     }
 }
